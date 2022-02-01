e005c461620661fb68ee8dd807a763553493c616
MiyamuP <pazu119@icloud.com>
2022-02-02 00:16:21 +0900

    setting

--- wywl_app/database/migrations/2022_02_01_133234_create_wants_table.php
+++ wywl_app/database/migrations/2022_02_01_133234_create_wants_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateWantsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('wants', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->integer('target_amount');
-            $table->rememberToken();
+            $table->string('want_name');
+            $table->integer('want_amount');
+            $table->string('want_image');
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('wants');
     }
 }
